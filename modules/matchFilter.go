774917d35642a79f33dba06d69a3ad2b1b7099bc
farshadr1 <farshadravaee@gmail.com>
2026-08-22 12:51:31 +0330

    Add matchfilter

---
+++ modules/matchFilter.go
@@ -0,0 +1,76 @@
+package modules
+
+import (
+	"math/cmplx"
+
+	"gonum.org/v1/gonum/dsp/fourier"
+)
+
+// MatchedFilter with iFFT implementation
+type MatchedFilter struct {
+	fftLen int
+	fft    *fourier.CmplxFFT
+	H      []complex128 // precomputed FFT of the flipped-conjugated reference pulse
+	outLen int
+	hLen   int
+}
+
+func nextPow2(n int) int {
+	p := 1
+	for p < n {
+		p *= 2
+	}
+	return p
+}
+
+// Constructure of MatchedFilter
+func NewMatchedFilter(signalLen int, refPulse Signal) *MatchedFilter {
+	hLen := len(refPulse.Samples)
+	outLen := signalLen + hLen - 1
+	fftLen := nextPow2(outLen)
+
+	// h[n] = conj(x[M-1-n])  (flip + conjugate the reference pulse)
+	h := make([]complex128, hLen)
+	for i, v := range refPulse.Samples {
+		h[hLen-1-i] = cmplx.Conj(v)
+	}
+
+	hPadded := make([]complex128, fftLen)
+	copy(hPadded, h)
+
+	fft := fourier.NewCmplxFFT(fftLen)
+	H := fft.Coefficients(nil, hPadded)
+
+	return &MatchedFilter{
+		fftLen: fftLen,
+		fft:    fft,
+		H:      H,
+		outLen: outLen,
+		hLen:   hLen,
+	}
+}
+
+// Apply Match Filter on signal
+func (mf *MatchedFilter) Apply(signal []complex128) []complex128 {
+	sigPadded := make([]complex128, mf.fftLen)
+	copy(sigPadded, signal)
+
+	X := mf.fft.Coefficients(nil, sigPadded)
+
+	Y := make([]complex128, mf.fftLen)
+	for i := range Y {
+		Y[i] = X[i] * mf.H[i]
+	}
+
+	// inverse FFT
+	y := mf.fft.Sequence(nil, Y)
+
+	// Normalize
+	norm := complex(1.0/float64(mf.fftLen), 0)
+	for i := range y {
+		y[i] *= norm
+	}
+
+	// trim output
+	return y[:mf.outLen]
+}
